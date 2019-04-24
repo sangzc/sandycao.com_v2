1c398cdaf5f8198547b575932e59f77cf7cea690
Sandy <sangzicao@gmail.com>
2019-04-23 21:54:10 -0700

    clear out gatsby starter code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Header from "../components/Header"
 
 const IndexPage = () => (
-  <Layout>
-    <h1>sandy cao</h1>
+  <>
+    <Header />
     <p>Welcomeeee.</p>
     <p>here's a little more about me.</p>
-  </Layout>
+  </>
 )
 
 export default IndexPage
